d910c68d48235a7338739a85d3bfbda5eb057794
Dylan Smit <95217570+SirCodeye@users.noreply.github.com>
2024-10-24 21:19:29 +0200

    Feat: implemented the TaskQueueSystem

--- src/main/java/io/dcsmit/task_queue_system/Main.java
+++ src/main/java/io/dcsmit/task_queue_system/Main.java
@@ -1,6 +1,20 @@
 package io.dcsmit.task_queue_system;
 
 public class Main {
+    private static final int MAX_CONCURRENT_TASKS = 3;
+
     public static void main(String[] args) {
+        TaskQueueManager taskQueueManager = new TaskQueueManager(MAX_CONCURRENT_TASKS);
+
+        taskQueueManager.enqueueTasks(new Task[]
+                {
+                        new Task(1, 1000),
+                        new Task(2,100),
+                        new Task(3,4000),
+                        new Task(4,2000),
+                        new Task(5,300)
+                });
+
+        taskQueueManager.performTasks();
     }
 }
